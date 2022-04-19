01fa1d9e5c02b200af6676059469064da1cbfe3a
Iqbal Ardiansyah <iardiansyah001@gmail.com>
2022-04-19 22:44:13 +0700

    Initial commit

---
+++ app/src/main/java/com/example/uts_iqbalardiansyah/ReportActivity.java
@@ -0,0 +1,14 @@
+package com.example.uts_iqbalardiansyah;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReportActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report);
+    }
+}
